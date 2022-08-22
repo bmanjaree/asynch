3568aa3a45f1b845ff6d4d82bcd79e6b7883724a
Felipe Quintero <fquintero@gmail.com>
2022-08-22 11:40:05 -0500

    model402

--- src/models/equations.c
+++ src/models/equations.c
@@ -2890,7 +2890,7 @@ void model402(double t, \
 	        for (i = 0; i < num_parents; i++)
 	            ans[STATE_DISCHARGE] += y_p[i * dim + STATE_DISCHARGE];
 	        ans[STATE_DISCHARGE] = invtau * pow(q, lambda_1) * ans[STATE_DISCHARGE];    // discharge[0]
-            dam_storage=1e-6;
+            dam_storage=1e-1;
             ans[STATE_DAM_STORAGE] = -dam_storage;
 
         }
